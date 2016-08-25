a8494d0c81fb3ba3b8274a3b89842845c28d31f0
Colin But <colinbut@users.noreply.github.com>
2016-08-25 20:57:09 +0100

    initial code

---
+++ src/main/java/com/mycompany/guice/gs/DatabaseTransactionLog.java
@@ -0,0 +1,10 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2016 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.guice.gs;
+
+public class DatabaseTransactionLog implements TransactionLog {
+
+}
---
+++ src/main/java/com/mycompany/guice/gs/model/card/BankCard.java
@@ -0,0 +1,10 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2016 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.guice.gs.model.card;
+
+public abstract class BankCard {
+
+}
