3c7065073ea18d2e7f946d4c156d3bcfcc912c0e
Colin But <colinbut@users.noreply.github.com>
2016-08-25 21:13:36 +0100

    linked bindings

---
+++ src/main/java/com/mycompany/guice/gs/core/transaction/MySQLDatabaseTransactionLog.java
@@ -0,0 +1,11 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2016 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.guice.gs.core.transaction;
+
+public class MySQLDatabaseTransactionLog extends DatabaseTransactionLog {
+
+
+}
--- src/main/java/com/mycompany/guice/gs/modules/BillingModule.java
+++ src/main/java/com/mycompany/guice/gs/modules/BillingModule.java
@@ -9,13 +9,18 @@ import com.google.inject.AbstractModule;
 import com.mycompany.guice.gs.core.payment.CreditCardProcessor;
 import com.mycompany.guice.gs.core.transaction.DatabaseTransactionLog;
 import com.mycompany.guice.gs.core.payment.PaypalCreditCardProcessor;
+import com.mycompany.guice.gs.core.transaction.MySQLDatabaseTransactionLog;
 import com.mycompany.guice.gs.core.transaction.TransactionLog;
 
 public class BillingModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        bind(TransactionLog.class).to(DatabaseTransactionLog.class);
+
+        // linked bindings
         bind(CreditCardProcessor.class).to(PaypalCreditCardProcessor.class);
+        bind(TransactionLog.class).to(DatabaseTransactionLog.class);
+        bind(DatabaseTransactionLog.class).to(MySQLDatabaseTransactionLog.class); //chaining linked bindings
+
     }
 }
