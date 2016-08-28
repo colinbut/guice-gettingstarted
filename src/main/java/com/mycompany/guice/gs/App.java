f5cb4c6fbaf30b6f01c83f103cf67f12c33c19b1
Colin But <colinbut@users.noreply.github.com>
2016-08-28 09:14:01 +0100

    getting instance back from the guice injector

--- src/main/java/com/mycompany/guice/gs/App.java
+++ src/main/java/com/mycompany/guice/gs/App.java
@@ -5,11 +5,12 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.mycompany.guice.gs.config.modules.BillingModule;
 import com.mycompany.guice.gs.service.BillingService;
+import com.mycompany.guice.gs.service.SimpleBillingServiceImpl;
 
 public class App {
 
     public static void main( String[] args ) {
         Injector injector = Guice.createInjector(new BillingModule());
-        BillingService billingService = injector.getInstance(BillingService.class);
+        BillingService billingService = injector.getInstance(SimpleBillingServiceImpl.class);
     }
 }
